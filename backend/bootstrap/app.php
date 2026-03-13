bee36e044552cebc1063b3ae9e27fdf0b69d772d
Gáspár Róbert <grobi447@gmail.com>
2026-03-13 18:35:48 +0100

    input sanitization middleware

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SanitizeInput;
 use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: ['api/*']);
         $middleware->append(SecurityHeaders::class);
+        $middleware->append(SanitizeInput::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
